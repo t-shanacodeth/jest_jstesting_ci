b736f093f6eebe45c93ed1247cadf5f5856d41a5
Tishana B. <iw547idw@students.codeinstitute.net>
2025-02-24 21:56:50 +0000

    add abilty to read entire index.html and rund multiple tests in said html document

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,13 +6,21 @@
 const buttonClick = require('../button');
 
 beforeEach(() => {
-    document.body.innerHTML = '<p id="par"></p>';
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
 });
 
 describe('DOM tests', () => {
     test('expects p content to change', () => {
         buttonClick();
         expect(document.getElementById('par').innerHTML).toEqual('You Clicked');
+    });
+    test('h1 exists', () => {
+        expect(document.getElementsByTagName('h1')).toBeTruthy();
+        //can also use: expect(document.getElementByTagName('h1').length).toBe(1);
     });
 });
 
